2b620fe416995a976b41c2b722ee3f2c55d81fff
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-02-03 16:53:50 +0800

    Feat: take in input string and n number, output string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(input, n) {
+    let output = '';
+    if(n<0) return 'ERROR';
+    while(n>0) {
+        output += input;
+        n--;
+    }
+    return output;
 };
 
 // Do not edit below this line
