888624da705c2a35491943b5135ab6b8a25be23d
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-02-03 17:37:15 +0800

    Define function to take in 1 array and 1 number and return array without number

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    let inputArray = args[0];
+    let output = [];
+    let addFlag = False;
 
+    for(i=0;i<inputArray.length;i++) {
+        
+        if(inputArray[i] != args[1]) {
+            output.push(inputArray[i]);
+        }
+    }
+    return output;
 };
 
 // Do not edit below this line
