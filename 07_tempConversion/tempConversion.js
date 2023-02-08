3780dad87e9aa00fb02ffcb48d4d480069e3336c
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-02-08 11:14:31 +0800

    functions: convert input to F or C (two separate functions)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInput) {
+  return Math.round(10*(tempInput - 32) * 5/9)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInput) {
+  return Math.round(10*(tempInput * 9/5 + 32))/10;
 };
 
 // Do not edit below this line
