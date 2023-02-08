bd86069a05fe2df2e5f4f73782eeb9467c60f990
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-02-08 10:58:40 +0800

    feature: add error checking for negative, non number inputs

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
 const sumAll = function(x, y) {
     let total = 0;
+
+    if(x < 0 || y < 0) {
+        return 'ERROR';
+    }
+    if(typeof x != 'number' || typeof y != 'number'){
+        return 'ERROR';
+    }
+
     for(let i=x;i<=y;i++){
         total+=i;
     }
