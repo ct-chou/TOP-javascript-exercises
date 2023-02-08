53a7afdd6e47ad60dfe79ae35c2f27effc881463
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-02-08 11:01:18 +0800

    Feat: enable y to be smaller than x

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,21 @@
 const sumAll = function(x, y) {
     let total = 0;
+    let start = x;
+    let end = y;
 
     if(x < 0 || y < 0) {
         return 'ERROR';
     }
+    
     if(typeof x != 'number' || typeof y != 'number'){
         return 'ERROR';
     }
 
-    for(let i=x;i<=y;i++){
+    if(y<x){
+        start = y;
+        end = x;
+    }
+    for(let i=start;i<=end;i++){
         total+=i;
     }
     return total;
