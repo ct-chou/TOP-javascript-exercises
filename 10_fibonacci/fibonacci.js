1f0401653f0c354d7e0480bf07bf09663b7e4ee6
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-03-16 19:17:31 +0800

    completed exercises

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    let fib = [1, 1];
+    
+    if(+number<1) {
+        return 'OOPS';
+    }
+    else if(+number <= 2) {
+        return fib[number-1];
+    }
+    else {
+        for(i=2;i<+number;i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+        return fib[+number-1];
+    }
 };
 
 // Do not edit below this line
