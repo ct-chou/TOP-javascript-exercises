1b80a8a35ed2070cac1ce12741993ade6f1a64af
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-03-16 19:57:34 +0800

    completed exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,28 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    // const sortAge = people.sort((a,b) =>
+    //     ((a.yearOfDeath - a.yearOfBirth) < (b.yearOfDeath - b.yearOfBirth)) ? 1: -1);
+    const sortAge = people.sort((a,b) => {
+        const thisYear = new Date().getFullYear();
+        if(a.yearOfDeath == undefined) {
+            ageA = thisYear - a.yearOfBirth;
+        }
+        else {
+            ageA = a.yearOfDeath - a.yearOfBirth;
+        }
+        if(b.yearOfDeath == undefined) {
+            ageB = thisYear - b.yearOfBirth;
+        }
+        else {
+            ageB = b.yearOfDeath - b.yearOfBirth;
+        }
+        if(ageA < ageB) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    });
+    return sortAge[0];
 };
 
 // Do not edit below this line
