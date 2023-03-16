b8ac3a7bf928c1c3c686b001cb00748ff3024a69
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-03-16 18:10:12 +0800

    completed exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(...args) {
+	return args[0] + args[1];
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+  if(input === undefined || input.length == 0)
+    return 0;
+  else 
+    return input.reduce((total, number) => total+number,0);
+}
+
+
+const multiply = function(input) {
+  if(input === undefined || input.length == 0)
+    return 0;
+  else 
+    return input.reduce((total, number) => total*number,1);
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const power = function(x, y) {
+	return x**y;
+}
+const factorial = function(x) {
+	if(x==0) return 1;
+  else {
+    let factorial = 1;
+    for(i=x;i>0;i--){
+      factorial *= i;
+    }
+    return factorial;
+  }
 };
 
 // Do not edit below this line
