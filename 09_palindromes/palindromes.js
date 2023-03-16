8391686b24fc995120ee5b12b827c0cf88cc0219
ct-chou <63345621+ct-chou@users.noreply.github.com>
2023-03-16 18:43:42 +0800

    completed exercises

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let cleanedInput = input.toLowerCase().replace(/\W/g,'');
+    let reverse = '';
+    for(i=cleanedInput.length-1; i>=0; i--){
+        reverse += cleanedInput[i];
+    }
+    return reverse == cleanedInput;
 };
 
 // Do not edit below this line
